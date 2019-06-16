34c30d5bf55b9ab2630789a07cdbe1f67ba1e077
J Frost <icefang@hotmail.com>
2019-06-16 14:45:39 -0700

    add movie-this command

--- liri.js
+++ liri.js
@@ -4,9 +4,78 @@ require("dotenv").config();
 // import key.js and store its variables.
 var keys = require("./key.js");
 
-// setup access key information.
-var spotify = new spotify(keys.spotify);
-// now spotify is available to use.
+// setup spotify package
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+
+// Grab the axios package...
+var axios = require("axios");
 
 var commandStr = process.argv[2];
+
+// creating titleStr 
+var titleStr = process.argv[3];
+if (process.argv.length > 4) {
+    for (var i = 4; i < process.argv.length; i++) {
+        titleStr = titleStr + "+" + process.argv[i];
+    }
+}
+
+function logMovie(response) {
+    if (response.data.Response=="False") {
+        console.log("Movie Not Found!");
+        return;
+    }
+    console.log(response.data);
+    console.log("Title: " + response.data.Title );
+    console.log("Year Released : " + response.data.Year);
+    console.log("IMDB Rating " + response.data.imdbRating);
+    var tomatoRating = "N/A";
+    for(var i=0; i<response.data.Ratings.length;i++) {
+        if (response.data.Ratings[i].Source === "Rotten Tomatoes") {
+            tomatoRating = response.data.Ratings[i].Value;
+            break;
+        }
+    }
+    console.log("Rotten Tomatoes Rating : " + tomatoRating );
+    console.log("Country : " + response.data.Country );
+    console.log("Language : " + response.data.Language);
+    console.log("Plot : " + response.data.Plot);
+    console.log("Actors : " + response.data.Actors);
+}
+
+function logError(error) {
+    if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+    } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an object that comes back with details pertaining to the error that occurred.
+        console.log(error.request);
+    } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+    }
+    console.log(error.config);
+}
+
+switch (commandStr) {
+    case "concert-this":
+        var queryURL = "https://rest.bandsintown.com/artists/" + titleStr + "/events?app_id=codingbootcamp";
+        break;
+    case "spotify-this-song":
+        break;
+    case "movie-this":
+        var queryURL = "http://www.omdbapi.com/?t=" + titleStr + "&y=&plot=short&apikey=trilogy";
+        axios.get(queryURL).then(logMovie).catch(logError);
+        break;
+    case "do-what-it-says":
+        break;
+    default:
+        console.log("invalid command!");
+        break;
+}
 
