48b789f9a92fa4b9eac9926e5632c52859642a45
J Frost <icefang@hotmail.com>
2019-06-16 15:22:44 -0700

    add concer-this

--- liri.js
+++ liri.js
@@ -4,6 +4,8 @@ require("dotenv").config();
 // import key.js and store its variables.
 var keys = require("./key.js");
 
+var moment = require('moment');
+
 // setup spotify package
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
@@ -22,26 +24,39 @@ if (process.argv.length > 4) {
 }
 
 function logMovie(response) {
-    if (response.data.Response=="False") {
+    if (response.data.Response == "False") {
         console.log("Movie Not Found!");
         return;
     }
-    console.log(response.data);
-    console.log("Title: " + response.data.Title );
+    // console.log(response.data);
+    console.log("Title: " + response.data.Title);
     console.log("Year Released : " + response.data.Year);
     console.log("IMDB Rating " + response.data.imdbRating);
     var tomatoRating = "N/A";
-    for(var i=0; i<response.data.Ratings.length;i++) {
+    for (var i = 0; i < response.data.Ratings.length; i++) {
         if (response.data.Ratings[i].Source === "Rotten Tomatoes") {
             tomatoRating = response.data.Ratings[i].Value;
             break;
         }
     }
-    console.log("Rotten Tomatoes Rating : " + tomatoRating );
-    console.log("Country : " + response.data.Country );
+    console.log("Rotten Tomatoes Rating : " + tomatoRating);
+    console.log("Country : " + response.data.Country);
     console.log("Language : " + response.data.Language);
     console.log("Plot : " + response.data.Plot);
     console.log("Actors : " + response.data.Actors);
+}
+
+function logConcert(response) {
+    // console.log(response.data);
+    console.log(response.data.length + " events found!");
+    for(var i=0; i<response.data.length; i++) {
+        console.log("event " + (i+1));
+        console.log("   Venue : " + response.data[i].venue.name);
+        console.log("     City    : " + response.data[i].venue.city);
+        console.log("     Region  : " + response.data[i].venue.region);
+        console.log("     Country : " + response.data[i].venue.country);
+        console.log("   Date of the event : " + moment(response.data[i].datetime).format("MM/DD/YYYY"));
+    }
 }
 
 function logError(error) {
@@ -65,6 +80,8 @@ function logError(error) {
 switch (commandStr) {
     case "concert-this":
         var queryURL = "https://rest.bandsintown.com/artists/" + titleStr + "/events?app_id=codingbootcamp";
+        console.log(queryURL);
+        axios.get(queryURL).then(logConcert).catch(logError);
         break;
     case "spotify-this-song":
         break;
